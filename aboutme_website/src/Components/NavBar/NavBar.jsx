42a3b8ab59dca64dd5194969d1a51eb03a3488c3
WCalderbank <wcalderbank99@gmail.com>
2022-04-21 10:06:15 +0100

    navbar functional

--- aboutme_website/src/Components/NavBar/NavBar.jsx
+++ aboutme_website/src/Components/NavBar/NavBar.jsx
@@ -8,26 +8,52 @@ import {
   FaFingerprint,
   FaEnvelopeSquare,
 } from "react-icons/fa";
+import { useState } from "react";
 
 function NavBar() {
+  const [currentNav, setCurrentNav] = useState("#");
   return (
     <nav>
-      <a href="#home">
+      <a
+        href="#"
+        onClick={() => setCurrentNav("#")}
+        className={currentNav === "#" ? "active" : ""}
+      >
         <FaHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setCurrentNav("#about")}
+        className={currentNav === "#about" ? "active" : ""}
+      >
         <FaFingerprint />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setCurrentNav("#experience")}
+        className={currentNav === "#experience" ? "active" : ""}
+      >
         <FaLaptopCode />
       </a>
-      <a href="#portfolio">
+      <a
+        href="#portfolio"
+        onClick={() => setCurrentNav("#portfolio")}
+        className={currentNav === "#portfolio" ? "active" : ""}
+      >
         <FaFolderOpen />
       </a>
-      <a href="#references">
+      <a
+        href="#references"
+        onClick={() => setCurrentNav("#references")}
+        className={currentNav === "#references" ? "active" : ""}
+      >
         <FaUsers />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setCurrentNav("#contact")}
+        className={currentNav === "#contact" ? "active" : ""}
+      >
         <FaEnvelopeSquare />
       </a>
     </nav>
