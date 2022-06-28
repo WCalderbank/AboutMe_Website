ac6853223f343064cdb02ee7d0b6333e081fb742
WCalderbank <wcalderbank99@gmail.com>
2022-06-28 21:35:18 +0100

    Portfolio Section

--- aboutme_website/src/Components/Portfolio/Portfolio.jsx
+++ aboutme_website/src/Components/Portfolio/Portfolio.jsx
@@ -1,8 +1,256 @@
 import React from "react";
 import "./Portfolio.css";
+import Project1 from "../../Accessories/React-Built D&D - 1.png";
 
 function Portfolio() {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Projects</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio_container">
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_project">
+          <div className="portfolio_project_image">
+            <img src={Project1} alt="Project" />
+          </div>
+          <h3>Portfolio Project Title</h3>
+          <div className="portfolio_project_cta">
+            <a
+              href="https://github.com/WCalderbank/Random-Dungeons-and-Dragons-Character-Creator"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com/WCalderbank"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio;
